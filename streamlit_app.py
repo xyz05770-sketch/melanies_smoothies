f2e2bddb731e9618e35d3467e573f01419517c3d
xyz05770-sketch <xyz05770@gmail.com>
2025-08-26 18:30:40 +0530

    Update streamlit_app.py
    Testing the search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_data_frame, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 6 ingredients:',
