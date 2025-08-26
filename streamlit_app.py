3ecb6cfea14c3220d171c37d82c273e597aaa471
xyz05770-sketch <xyz05770@gmail.com>
2025-08-26 18:17:47 +0530

    Update streamlit_app.py
    Displays nutrition information for fruit chosen by user

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,13 @@ ingredients_list = st.multiselect(
     max_selections=6
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
